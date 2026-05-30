9fc83c57de9d61bef1c97d530ded4a2663cc2277
Anubhav Yadav <yadavanubhav2024@gmail.com>
2026-05-30 11:43:11 +0530

    Plus one

---
+++ 0-Array/8-Plus-one.py
@@ -0,0 +1,28 @@
+"""
+LeetCode 66: Plus one
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+"""
+
+def plusOne(digits):
+    digits = digits[::-1]
+    one, i = 1, 0
+
+    while one:
+        if i < len(digits):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                one = 0
+        else:
+            digits.append(1)
+            one = 0
+        i += 1
+    return digits[::-1]
+
+digits = [1,2,3]
+result = plusOne(digits)
+print(result)
